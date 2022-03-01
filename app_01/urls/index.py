32076696beec41f17ad3383fcf66e306c676e4c0
飞机炸蛋 <10420576+AircraftBoom@user.noreply.gitee.com>
2022-03-02 00:26:57 +0800

    make first_page

--- app_01/urls/index.py
+++ app_01/urls/index.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from app_01.views.home_page import home_page
+from app_01.views.index import home_page
 
 urlpatterns = [
     path("", home_page, name="home_page"),
--- app_01/views/home_page.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def home_page(request):
-    return HttpResponse("this is home page")
